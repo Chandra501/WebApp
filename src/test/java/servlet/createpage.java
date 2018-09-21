418220d5284746dee510d191b830af4d39d2f592
Chandra501 <43408864+Chandra501@users.noreply.github.com>
2018-09-21 14:53:21 +0530

    Update createpage.java

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -8,7 +8,7 @@ import javax.sql.*;
 
 public class createpage extends HttpServlet {
 	
-	public void doPost(HttpServletRequest req,HttpServletResponse res)
+	public void doPost(HttpServletRequest req,HttpServletResponse  res)
     throws ServletException, IOException{
 	try{
 		
